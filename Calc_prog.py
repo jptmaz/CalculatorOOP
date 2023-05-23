41193afa5c575afc65bb1a02b62979364191d783
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-23 11:45:43 +0800

    Difference method added (Operations changed)

--- Calc_prog.py
+++ Calc_prog.py
@@ -14,12 +14,16 @@ class calculator():
         print(message)
     # Create method for subtraction
     def difference(self, number_1, number_2):
-        difference = number_1 + number_2
+        difference = number_1 - number_2
         message = f"The difference of {number_1} and {number_2} is {difference}"
         print(message)
     # Create method for multiplication
     def product(self, number_1, number_2):
-        product = number_1 + number_2
+        product = number_1 * number_2
         message = f"The product of {number_1} and {number_2} is {product}"
         print(message)
     # Create method for division
+    def quotient(self, number_1, number_2):
+        quotient = number_1 / number_2
+        message = f"The sum of {number_1} and {number_2} is {quotient}"
+        print(message)
