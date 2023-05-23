1692aaac1ded984fccc176dfee61fa773f9efbe2
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-23 13:42:31 +0800

    More changes added

--- Calc_main.py
+++ Calc_main.py
@@ -8,39 +8,46 @@ def main():
     
     while(True):
         
-        print("""Welcome to Calkie your program calculator! 
-To begin, Calkie is asking you for two numbers!""")
-        # Ask user for numbers 1 & 2
-        number_1 = ui.input_user()
-        number_2 = ui.input_user()
+        print("Welcome to Calkie your program calculator!")
+        print("Do you want to use Calkie?")
+        Calkie_command = input(str("= ")).upper()
+        if Calkie_command == "YES":
+            print("To begin, Calkie is asking you for two numbers!""")
+            # Ask user for numbers 1 & 2
+            number_1 = ui.input_user()
+            number_2 = ui.input_user()
     
-        # Ask user for operation
-        print ("""[[[''''''' OPERATIONS ''''''']]]
+            # Ask user for operation
+            print ("""[[[''''''' OPERATIONS ''''''']]]
     1. Addition
     2. Subtraction
     3. Multiplication
     4. Division
     5. Exit""")
     
-        command = input(str("Enter the number of the operation that you wish to do: "))
-        if command == "1":
-            Calc.addition(number_1, number_2)
-        elif command == "2":
-            Calc.subtraction(number_1, number_2)
-        elif command == "3":
-            Calc.multiplication(number_1, number_2)
-        elif command == "4":
-            Calc.division(number_1, number_2)
-
-        elif command == "5":
+            command = input(str("Enter the number of the operation that you wish to do: "))
+            if command == "1":
+                Calc.addition(number_1, number_2)
+            elif command == "2":
+                Calc.subtraction(number_1, number_2)
+            elif command == "3":
+                Calc.multiplication(number_1, number_2)
+            elif command == "4":
+                Calc.division(number_1, number_2)
+            elif command == "5":
+                break
+            else:
+                print("I do not understand would you like to try again? YES or NO?")
+                tryagain_calc = input("= ").upper()
+                if tryagain_calc == "YES":
+                        True
+                elif tryagain_calc == "NO":
+                    break
+        elif Calkie_command == "NO":
             break
         else:
-            print("I do not understand would you like to try again? YES or NO?")
-            tryagain_calc = input("= ").upper()
-            if tryagain_calc == "YES":
-                True
-            elif tryagain_calc == "NO":
-                break
+            print("I don't understand. Please try again.")
+            True
 
 if __name__ == "__main__":
     main()
--- Calc_userinput.py
+++ Calc_userinput.py
@@ -4,7 +4,8 @@ class User_Input():
     def input_user(self):
         try:
             number = float(input("Enter the number: "))
-            return number
         except ValueError:
             print("An error has occured, please try again.")
             number = float(input("Enter a number again: "))
+        
+        return number
