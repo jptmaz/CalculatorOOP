1629970f5bcd586e9408e41bb231146a93828e74
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-23 09:30:07 +0800

    Main program and test program added

---
+++ Calc_prog.py
@@ -0,0 +1,6 @@
+# Create a class
+    # Create a special init method
+    # Create method for addition operation
+    # Create method for subtraction
+    # Create method for multiplication
+    # Create method for division
