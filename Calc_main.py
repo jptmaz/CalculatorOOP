bb720cc3f80324c1b1c813ee556e9faf210da8dd
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-23 11:40:21 +0800

    Several changes staged

--- Calc_main.py
+++ Calc_main.py
@@ -1,1 +1,11 @@
 # Import class
+from Calc_userinput import User_Input
+from Calc_prog import calculator
+
+ui = User_Input()
+
+# Ask user for numbers 1 & 2
+number_1 = ui.input_user()
+number_2 = ui.input_user()
+
+# Asl user fot 
--- Calc_userinput.py
+++ Calc_userinput.py
@@ -1,2 +1,6 @@
 # Create a class
+class User_Input():
     # Create method for first number 
+    def input_user(self):
+        number = float(input("Enter the number: "))
+        return number
