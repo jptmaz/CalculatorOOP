9b5a9774649fba0d5cf0a8ca04fcd7cc1cbc2053
Jasmin Paige <jaspagmaz98@gmail.com>
2023-06-03 20:48:45 +0800

    Added more to the i_calculator.py file

--- i_calculator.py
+++ i_calculator.py
@@ -1,4 +1,14 @@
 from Calc_prog import calculator
 
 class Calculator_Prog(calculator):
-    pass
+    def addition(self, number_1, number_2):
+        return super().addition(number_1, number_2)
+    
+    def subtraction(self, number_1, number_2):
+        return super().subtraction(number_1, number_2)
+    
+    def multiplication(self, number_1, number_2):
+        return super().multiplication(number_1, number_2)
+    
+    def division(self, number_1, number_2):
+        return super().division(number_1, number_2)
